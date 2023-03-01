56093438f9e0e5879374c2885fa64375006808ff
loic leforestier <loic.leforestier@mmibordeaux.com>
2023-03-01 11:19:39 +0100

    redirection vers films.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :films
+  root "films#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/films", to: "films#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
