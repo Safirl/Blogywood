15aff7d32e7b1b4316d1138e1f5ab9065738fc74
loic leforestier <loic.leforestier@mmibordeaux.com>
2023-03-01 19:40:49 +0100

    ajout de bootstrap et simple form + header

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,2 +1,6 @@
 class Film < ApplicationRecord
+  validates :title, presence: true
+  validates :summary, presence: true, length: { minimum: 10 }
+  validates :image, presence: true
+  validates :rating, presence: true
 end
